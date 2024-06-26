2bc4d7f93dc8feb5f6fa48f241dd86ae3d4651d6
Midori <midori@Midoris-MacBook-Pro.local>
2024-06-26 15:29:54 -0400

    Layout of my nfts are in rows

--- opend/src/opend_assets/src/components/Gallery.jsx
+++ opend/src/opend_assets/src/components/Gallery.jsx
@@ -21,8 +21,9 @@ function Gallery(props) {
       <h3 className="makeStyles-title-99 Typography-h3">{props.title}</h3>
       <div className="disGrid-root disGrid-container disGrid-spacing-xs-2">
         <div className="disGrid-root disGrid-item disGrid-grid-xs-12">
-          <div className="disGrid-root disGrid-container disGrid-spacing-xs-5 disGrid-justify-content-xs-center"></div>
-          {items}
+          <div className="disGrid-root disGrid-container disGrid-spacing-xs-5 disGrid-justify-content-xs-center">
+            {items}
+          </div>
         </div>
       </div>
     </div>
