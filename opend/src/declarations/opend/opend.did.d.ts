b074e12377b1c8914f0d9d97a49b2aae653689de
Midori <midori@Midoris-MacBook-Pro.local>
2024-06-25 21:18:36 -0400

    Creating actor class NFT

--- opend/src/declarations/opend/opend.did.d.ts
+++ opend/src/declarations/opend/opend.did.d.ts
@@ -1,11 +1,2 @@
 import type { Principal } from '@dfinity/principal';
-export interface _SERVICE {
-  'getListedNFTPrice' : (arg_0: Principal) => Promise<bigint>,
-  'getListedNFTs' : () => Promise<Array<Principal>>,
-  'getOpenDCanisterID' : () => Promise<Principal>,
-  'getOriginalOwner' : (arg_0: Principal) => Promise<Principal>,
-  'getOwnedNFTs' : (arg_0: Principal) => Promise<Array<Principal>>,
-  'isListed' : (arg_0: Principal) => Promise<boolean>,
-  'listItem' : (arg_0: Principal, arg_1: bigint) => Promise<string>,
-  'mint' : (arg_0: Array<number>, arg_1: string) => Promise<Principal>,
-}
+export interface _SERVICE {}
--- opend/src/opend_assets/src/components/App.jsx
+++ opend/src/opend_assets/src/components/App.jsx
@@ -2,19 +2,13 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-//import Item from "./Item";
-import Minter from "./Minter";
+import homeImage from "../../assets/home-img.png";
 
 function App() {
-  //const NFTID = "rrkah-fqaaa-aaaaa-aaaaq-cai";
-
   return (
     <div className="App">
       <Header />
-      {/* <Minter /> */}
-      {/* <Item id={NFTID} />
-       */}
+      <img className="bottom-space" src={homeImage} />
       <Footer />
     </div>
   );
